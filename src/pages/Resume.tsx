ffdcbae19688b865003cd3a69a381336200e484c
Antonio <antoniosusio@gmail.com>
2025-06-11 11:38:13 +0200

    Added test images to resumes and surrounded them with a link to open the zoomed image in a new window

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,3 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import type { Resumes } from "../resume";
+import resume from "../resume";
+import { nanoid } from "nanoid";
+
 export default function Resume() {
-  return <h1>Resume Page here</h1>;
+  const [resumes, setResumes] = React.useState<Resumes[]>([]);
+
+  React.useEffect(() => {
+    setResumes(resume);
+  }, []);
+
+  const resumeEl = resumes.map((item) => (
+    <div key={nanoid()} className="resume-card flex flex-col">
+      <h2>{item.period}</h2>
+      <h3>
+        {item.schoolName} - {item.city} {item.country}
+      </h3>
+      {item.eqfLevel && <p>EQF Level - {item.eqfLevel}</p>}
+      <p>{item.description}</p>
+      <Link to={`${item.imageUrl}`} target="_blank">
+        <img
+          className="resume-img"
+          src={item.imageUrl}
+          alt={item.alternativeText}
+        />
+      </Link>
+    </div>
+  ));
+
+  return (
+    <section className="resume-section flex flex-col">
+      <h1 className="resume-heading flex">Resume</h1>
+      <div className="resumes-container flex flex-col">{resumeEl}</div>
+    </section>
+  );
 }
---
+++ src/resume.ts
@@ -0,0 +1,34 @@
+export interface Resumes {
+    period: string,
+    schoolName: string,
+    city?: string,
+    country?: string,
+    description: string,
+    eqfLevel?: number,
+    imageUrl?: string,
+    alternativeText: string
+}
+
+const resume: Resumes[] = [
+    {
+        period: "2014 - 2019",
+        schoolName: "ITIS Alessandro Volta",
+        city: "Naples,",
+        country: "Italy",
+        description: "High School Diploma in Computer Science and Telecommunications",
+        eqfLevel: 4,
+        imageUrl: "/FrontEndCertificate.png",
+        alternativeText: "High School Diploma Image"
+    },
+
+    {
+        period: "2023 - 2024",
+        schoolName: "Scrimba",
+        city: "Online platform",
+        description: "Frontend Developer Career Path certificate of completion",
+        imageUrl: "/FrontEndCertificate.png",
+        alternativeText: "Image of Frontend developer career path certificate"
+    }
+]
+
+export default resume;

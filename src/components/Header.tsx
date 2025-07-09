de44fb1146d6201128bc71afdbc17e25e169d78a
Antonio <antoniosusio@gmail.com>
2025-07-09 10:43:29 +0200

    changed header style , implemented lightbox for certificates images and adjusted certificates section style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,37 +12,43 @@ export default function Header() {
   }
 
   return (
-    <header className="header flex">
-      <div className="header-left-container">
-        <h1 className="heading-name flex">Antonio Susio</h1>
-        <h2 className="heading-role">Frontend developer</h2>
-        <div className="social-container flex">
-          <a href="https://github.com/AntonioSusio" target="_blank">
-            <FaGithub className="social-icon github-icon" />
-          </a>
-
-          <a
-            href="https://www.linkedin.com/in/antonio-susio-9b2089226/"
-            target="_blank"
-          >
-            <FaLinkedinIn className="social-icon linkedin-icon" />
-          </a>
-
-          <a href={`mailto:antoniosusio@gmail.com`}>
-            <SiGmail className="social-icon gmail-icon" />
-          </a>
-
-          <IoDocumentText className="social-icon cv-icon" />
+    <header className="header flex flex-col">
+      <div className="header-inner-container flex">
+        <div className="header-left-container">
+          <h1 className="heading-name flex">Antonio Susio</h1>
+          <h2 className="heading-role">Frontend developer</h2>
         </div>
+
+        <button onClick={openMenu} className="open-menu-button flex flex-col">
+          <span className="hamburger-menu-string-1"></span>
+          <span className="hamburger-menu-string-2"></span>
+          <span className="hamburger-menu-string-3"></span>
+        </button>
+
+        <Navbar menu={menu} setMenu={setMenu} />
       </div>
 
-      <Navbar menu={menu} setMenu={setMenu} />
+      <div className="social-container flex">
+        <a href="https://github.com/AntonioSusio" target="_blank">
+          <FaGithub className="social-icon github-icon" />
+        </a>
 
-      <button onClick={openMenu} className="open-menu-button flex flex-col">
-        <span className="hamburger-menu-string-1"></span>
-        <span className="hamburger-menu-string-2"></span>
-        <span className="hamburger-menu-string-3"></span>
-      </button>
+        <a
+          href="https://www.linkedin.com/in/antonio-susio-9b2089226/"
+          target="_blank"
+        >
+          <FaLinkedinIn className="social-icon linkedin-icon" />
+        </a>
+
+        <a href={`mailto:antoniosusio@gmail.com`}>
+          <SiGmail className="social-icon gmail-icon" />
+        </a>
+
+        <IoDocumentText className="social-icon cv-icon" />
+      </div>
     </header>
   );
+}
+
+{
 }
--- src/pages/Certificates.tsx
+++ src/pages/Certificates.tsx
@@ -7,18 +7,8 @@ import certificatesData from "../certificatesData";
 export default function Certificates(): JSX.Element {
   const [certificates, setCertificates] = React.useState<Certificate[]>([]);
   const [slideIndex, setSlideIndex] = React.useState<number>(0);
-
-  const certificatesEl: JSX.Element[] = certificates.map(
-    (certificate: Certificate): JSX.Element => (
-      <img
-        key={certificate.id}
-        src={certificate.certificateImg}
-        className="certificate-img"
-        alt={certificate.alternativeText}
-        style={{ translate: `${-100 * slideIndex}%` }}
-      />
-    )
-  );
+  const [lightBoxOpen, setLightBoxOpen] = React.useState<boolean>(false);
+  const [activeImg, setActiveImg] = React.useState<string>("");
 
   React.useEffect((): void => {
     if (
@@ -29,10 +19,43 @@ export default function Certificates(): JSX.Element {
     }
   }, [certificatesData]);
 
+  function openLightBox(imgUrl: string): void {
+    setActiveImg(imgUrl);
+    setLightBoxOpen(true);
+  }
+
+  function closeLightBox(): void {
+    setLightBoxOpen(false);
+    setActiveImg("");
+  }
+
+  const certificatesEl: JSX.Element[] = certificates.map(
+    (certificate: Certificate): JSX.Element => (
+      <img
+        key={certificate.id}
+        src={certificate.certificateImg}
+        className="certificate-img"
+        alt={certificate.alternativeText}
+        style={{ translate: `${-100 * slideIndex}%` }}
+        onClick={() => openLightBox(certificate.certificateImg)}
+      />
+    )
+  );
+
   return (
     <section className="certificates-section flex flex-col">
       <h1 className="certificates-heading flex">Certificates</h1>
       <Carousel certificates={certificatesEl} setSlideIndex={setSlideIndex} />
+
+      {lightBoxOpen && (
+        <div className="lightbox-overlay" onClick={closeLightBox}>
+          <img
+            src={activeImg}
+            alt={`certificate image enlarged`}
+            className="lightbox-image"
+          />
+        </div>
+      )}
     </section>
   );
 }
